4a9fbd7ead016bdbfd659f7380837e3c0837df3b
Nakul Ravindra Padalkar <2807210+nakulpadalkar@users.noreply.github.com>
2024-01-20 21:19:53 -0500

    Added docstrings to dataloader.py

--- models/dataloader.py
+++ models/dataloader.py
@@ -1,46 +1,28 @@
 import os
-import json
-import torch
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 
 
 class CustomDataLoader:
     OUTPUT_FOLDER = "output/image_output"
     IMAGE_SIZE = (224, 224)
-    MEAN_STD_FILE = "mean_std.json"
+    MEAN = [0.485, 0.456, 0.406]  # ImageNet mean
+    STD_DEV = [0.229, 0.224, 0.225]  # ImageNet std dev
 
     def __init__(self, base_folder="./datasets/Vision_data", batch_size=256, num_workers=6):
         self.base_folder = base_folder
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-        # Initialize mean and std
-        self.MEAN = [0.485, 0.456, 0.406]  # Default values
-        self.STD_DEV = [0.229, 0.224, 0.225]  # Default values
-
-        # Define transformations using the initialized or loaded mean and std
+        # Define transformations using ImageNet mean and std
         self.transform = transforms.Compose([
             transforms.Resize(self.IMAGE_SIZE),
             transforms.ToTensor(),
             transforms.Normalize(mean=self.MEAN, std=self.STD_DEV)
         ])
-        
+
         # Create data loaders
         self.train_loader, self.test_loader, self.validation_loader = self.create_data_loaders()
-
-        # Initialize or load mean and std
-        self._initialize_or_load_mean_std()
-
-    def _initialize_or_load_mean_std(self):
-        if os.path.exists(self.MEAN_STD_FILE):
-            with open(self.MEAN_STD_FILE, 'r') as file:
-                mean_std = json.load(file)
-            self.MEAN = mean_std['mean']
-            self.STD_DEV = mean_std['std']
-        else:
-            self.calculate_and_save_mean_std()
 
     def create_data_loaders(self):
         train_folder = os.path.join(self.base_folder, "train")
@@ -62,28 +44,6 @@ class CustomDataLoader:
             validation_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
 
         return train_loader, test_loader, validation_loader
-
-    def calculate_and_save_mean_std(self):
-        mean = torch.zeros(3)
-        std = torch.zeros(3)
-        num_samples = 0
-
-        for images, _ in tqdm(self.train_loader, desc="Calculating Mean and Std"):
-            batch_samples = images.size(0)
-            images = images.view(batch_samples, images.size(1), -1)
-            mean += images.mean(2).sum(0)
-            std += images.std(2).sum(0)
-            num_samples += batch_samples
-
-        mean /= num_samples
-        std /= num_samples
-
-        self.MEAN = mean.tolist()
-        self.STD_DEV = std.tolist()
-
-        mean_std_dict = {"mean": self.MEAN, "std": self.STD_DEV}
-        with open(self.MEAN_STD_FILE, "w") as file:
-            json.dump(mean_std_dict, file)
 
 
 # Example usage
